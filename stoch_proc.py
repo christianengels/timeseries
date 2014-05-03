cd48e23c3f358745b5f736133c456c89db99f805
Christian Engels <engels.chr@gmail.com>
2014-05-03 19:21:09 +0100

    second commit

--- stoch_proc.py
+++ stoch_proc.py
@@ -1,6 +1,6 @@
-# Copyright 2014 The University of Texas at Austin
+# FLAME Copyright 2014 The University of Texas at Austin
 #
-# For licensing information see
+# For FLAME licensing information see
 #                http://www.cs.utexas.edu/users/flame/license.html
 #
 # Programmed by: Christian Engels
@@ -11,8 +11,6 @@ import numpy as np
 import pylab as pl
 
 def ar(first, mu, alpha, x):
-
-    “”” Autoregressive process of order one “””
 
     xT, \
     xB  = flame.part_2x1(x, \
@@ -44,8 +42,6 @@ def ar(first, mu, alpha, x):
 
 
 def sp(length, first=0, mu=0, alpha=1, std=1):
-    
-   “”” Full stochastic process “””
 
     x = std * np.random.randn(length-1, 1)
     
@@ -53,15 +49,13 @@ def sp(length, first=0, mu=0, alpha=1, std=1):
     
     first = np.array([first])
     
-    x = np.append(first, x)
+    x = np.append(first, x).reshape(length, 1)
     
     return x
 
 
 def sp_many(length, rep=10, first=0, mu=0, alpha=1, std=1):
     
-    “”” Many stochastic processes “””
-
     for i in range(rep):
 
         x = sp(length, first, mu, alpha, std)
@@ -70,8 +64,44 @@ def sp_many(length, rep=10, first=0, mu=0, alpha=1, std=1):
 
     pl.show()
 
-    
-    
+
+def rtot(x):
+
+    xT, \
+    xB  = flame.part_2x1(x, \
+                         0, 'TOP')
+
+    sum = 0.0
+
+    while xT.shape[0] < x.shape[0]:
+
+        x0,   \
+        chi1, \
+        x2    = flame.repart_2x1_to_3x1(xT, \
+                                        xB, \
+                                        1, 'BOTTOM')
+
+        #------------------------------------------------------------#
+        chi1 += sum
+        sum = chi1
+        #------------------------------------------------------------#
+
+        xT, \
+        xB  = flame.cont_with_3x1_to_2x1(x0,   \
+                                         chi1, \
+                                         x2,   \
+                                         'TOP')
+
+    flame.merge_2x1(xT, \
+                    xB, x)
+
+def rmean(x):
+
+    m, n = np.shape(x)
+
+    rtot(x)
+
+    x = x * (1.0 * np.arange(1, (m+1)).reshape(m, 1))     
     
     
  
